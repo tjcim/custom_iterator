d60e2f43e703436b98782da9929472d1f9ae5424
Trevor Christiansen <trevor@tjc.im>
2022-06-25 05:32:52 -0600

    Updated

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
 
 @app.route("/<date>|<index>|<store>")
 def invoice(date, index, store):
-    if date == "20220426" and index == "23" and store == "02":
-        return render_template("winner.html")
+    if date == "20220421" and index == "24" and store == "04":
+        return render_template("winner.html", _date=date, index=index, store=store)
     elif date == "20211105" and index == "22" and store == "03":
         return render_template("example.html")
     else:
